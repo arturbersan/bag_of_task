a4f2c83800840c3842d797deded1c19bba0b6589
Artur Bersan de Faria <artur_bersan@hotmail.com>
2017-05-23 19:06:24 +0000

    update matriz size

--- django/celery_matrix/matriz_calculator.py
+++ django/celery_matrix/matriz_calculator.py
@@ -1,4 +1,5 @@
 # Code calculator here
+import datetime
 
 def read_matrix(matrix_file):
 	f = open(matrix_file, 'r')
@@ -11,23 +12,33 @@ def read_matrix(matrix_file):
 def print_matrix(M):
 	[print(element) for element in M]
 
-# Sample 3x3
-A = read_matrix('input/A.matrix')
+def oi():
+	#a = datetime.now()
+	# Sample 3x3
+	A = read_matrix('input/A.matrix')
 
 
-# Sample 3x4
-B = read_matrix('input/B.matrix')
+	# Sample 3x4
+	B = read_matrix('input/B.matrix')
 
-# Result is 3x4
-R = [[0, 0, 0, 0],
-	[0, 0, 0, 0],
-	[0, 0, 0, 0]]
+	# Result is 3x4
+	R = [[ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0],
+	    [ 0, 0,0,0,0,0,0,0]]
 
+	for i in range(len(A)):
+		# New matriz have same quantity of Y rows
+		for j in range(len(B[0])):
+			for k in range(len(B)):
+				R[i][j] += A[i][k] * B[k][j]
 
-for i in range(len(A)):
-	# New matriz have same quantity of Y rows
-	for j in range(len(B[0])):
-		for k in range(len(B)):
-			R[i][j] += A[i][k] * B[k][j]
+	print_matrix(R)
+	#print(datetime.now()-a)
 
-print_matrix(R)
+if __name__ == "__main__":
+	oi()
